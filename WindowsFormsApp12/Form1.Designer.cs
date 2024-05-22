fa3befbe3b09685d76fdb85382529788addd3ecd
Everlasting Despair <vlad.andrejkovets@gmail.com>
2024-05-23 00:33:39 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp12/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.findButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(845, 22);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // findButton
+            // 
+            this.findButton.Location = new System.Drawing.Point(12, 40);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(203, 32);
+            this.findButton.TabIndex = 1;
+            this.findButton.Text = "Старт";
+            this.findButton.UseVisualStyleBackColor = true;
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(221, 48);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(77, 16);
+            this.resultLabel.TabIndex = 2;
+            this.resultLabel.Text = "Результат";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 84);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.findButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button findButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ WindowsFormsApp12/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void findButton_Click(object sender, EventArgs e)
+        {
+            string inputText = inputTextBox.Text;
+            List<string> maxLengthWords = FindMaxLengthWords(inputText);
+            resultLabel.Text = "Слова максимальной длины: " + string.Join(", ", maxLengthWords);
+        }
+
+        private List<string> FindMaxLengthWords(string input)
+        {
+          
+            string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int maxLength = words.Max(word => word.Length);
+
+            List<string> maxLengthWords = words.Where(word => word.Length == maxLength).ToList();
+
+            return maxLengthWords;
+        }
+    }
+}
